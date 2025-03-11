d373068df872727f2acb5fbe6b45ce75e968edd3
cheva360 <134435764+cheva360@users.noreply.github.com>
2025-03-11 20:43:24 +0000

    changes

---
+++ scripts/c3main.js
@@ -0,0 +1,7 @@
+import "./../gl-matrix.js";
+import "./../gltfdata.js";
+import "./../gltfmodel.js";
+import "./../gltfmodelw.js";
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
